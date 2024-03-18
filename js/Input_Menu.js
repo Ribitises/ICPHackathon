8b712633666bcf3145886e4f152bcfd6893628cb
Rhymeses <cortezrhymeses@gmail.com>
2024-03-18 10:43:34 +0800

    Input Menu Initial

---
+++ js/Input_Menu.js
@@ -0,0 +1,63 @@
+// Monitoring Sheet
+
+let popupMS = document.getElementById("popupMS");
+
+function openMS(){
+    popupMS.classList.add("open-MS");
+}
+
+function submitMS(){
+    popupMS.classList.remove("open-MS");
+}
+
+function closeMS(){
+    popupMS.classList.remove("open-MS");
+}
+
+// TPR Sheet
+
+let popupTPR = document.getElementById("popupTPR");
+
+function openTPR(){
+    popupTPR.classList.add("open-TPR");
+}
+
+function submitTPR(){
+    popupTPR.classList.remove("open-TPR");
+}
+
+function closeTPR(){
+    popupTPR.classList.remove("open-TPR");
+}
+
+// Fluid Input Output Sheet
+
+let popupIO = document.getElementById("popupIO");
+
+function openIO(){
+    popupIO.classList.add("open-IO");
+}
+
+function submitIO(){
+    popupIO.classList.remove("open-IO");
+}
+
+function closeIO(){
+    popupIO.classList.remove("open-IO");
+}
+
+// Nurses Note Sheet
+
+let popupNN = document.getElementById("popupNN");
+
+function openNN(){
+    popupNN.classList.add("open-NN");
+}
+
+function submitNN(){
+    popupNN.classList.remove("open-NN");
+}
+
+function closeNN(){
+    popupNN.classList.remove("open-NN");
+}
