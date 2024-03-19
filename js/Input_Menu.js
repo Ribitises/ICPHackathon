b0240ac371b3b5ae2f8eb0d4a9b259ffabc992c5
Rhymeses <125356069+Ribitises@users.noreply.github.com>
2024-03-19 16:22:17 +0800

    Update Input Menu

--- js/Input_Menu.js
+++ js/Input_Menu.js
@@ -1,3 +1,21 @@
+// Tabs
+function openTab(evt, tabName){
+    var i, tabcontent, tab;
+
+    tabcontent = document.getElementsByClassName("tabContent");
+    for (i = 0; i < tabcontent.length; i++){
+        tabcontent[i].style.display = "none";
+    }
+
+    tab = document.getElementsByClassName("tab");
+    for (i = 0; i < tab.length; i++){
+        tab[i].className = tab[i].className.replace(" active", "");;
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += "active";
+}
+
 // Monitoring Sheet
 
 let popupMS = document.getElementById("popupMS");
